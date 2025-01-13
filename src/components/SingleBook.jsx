9f1d7540d6dc0a226ab57de4044eba4ad434f190
Ninfa-C <ninfa_95@hotmail.it>
2025-01-13 16:29:55 +0100

    css and price added

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -16,24 +16,29 @@ render(){
              <Card
           className={`mb-3 ${this.props.isSelected ? "border border-danger" : ""}`}
           style={{
-            height: "480px"}}
+            height: "510px"}}
             onClick={() => this.props.selected(this.props.book.asin)}
         >
                 <Card.Img variant="top" src={this.props.book.img} alt={this.props.book.title} style={{ height: "300px", objectFit: "cover" }} />
                 <Card.Body className="d-flex flex-column">
                     <div>
                         <Card.Title className="short">{this.props.book.title}</Card.Title>
-                        <Badge bg="warning" text="dark">
+                        <Badge bg="warning" text="dark" className="mb-2">
                             {this.props.book.category}
                         </Badge>
+                        
                     </div>
                     <CardGroup className="d-flex justify-content-center gap-2 mt-auto">
-                        <Button variant="danger" size="sm">
+                        <Card.Text className=" mb-2 text-start me-auto fst-italic">{this.props.book.price}€</Card.Text>
+                        <CardGroup className="d-flex justify-content-center gap-2 mt-auto">
+                            <Button variant="danger" size="sm">
                             <i className="bi bi-bag-plus" style={{ fontSize: "1rem" }}></i> Aggiungi
                         </Button>
                         <Button variant="outline-secondary" size="sm">
                             <i className="bi bi-bag-plus" style={{ fontSize: "1rem" }}></i> Scarta
                         </Button>
+                        </CardGroup>
+                        
                     </CardGroup>
                 </Card.Body>
             </Card>
